ec4c17b5c4b2c1ee524646de6ce8e93236f05c53
AminesCodes <aminebensalem@pursuit.org>
2020-03-07 18:56:54 -0500

    Contact section styled partially

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 export default function Contact(props) {
-    const upArrow = <img src={require('../assets/arrow-alt-circle-up-solid.svg')} alt='Up arrow' />
-    const downArrow = <img src={require('../assets/arrow-alt-circle-down-solid.svg')} alt='Down arrow' />
-    const homeIcon = <img src={require('../assets/home-solid.svg')} alt='Down arrow' />
+    const upArrow = <img 
+        src={require('../assets/arrow-alt-circle-up-solid.svg')} 
+        alt='Up arrow' 
+        width='50px' />;
+
+    const downArrow = <img 
+        src={require('../assets/arrow-alt-circle-down-solid.svg')} 
+        alt='Down arrow' 
+        width='50px' />;
+    
+    const homeIcon = <img 
+        src={require('../assets/home-solid.svg')} 
+        alt='Home icon' 
+        width='50px' />;
     
     const style = {
         background: `url(${require('../assets/contact.jpg')}) no-repeat center center fixed`,
@@ -16,9 +27,10 @@ export default function Contact(props) {
     
     return(
         <section className='container-fluid p-3' id='contact' style={style}>
-            <AnchorLink className='btn' href='#home'>Home</AnchorLink>
-            <AnchorLink className='btn' href='#home'>Home</AnchorLink>
-
+            <div className='d-flex justify-content-between'>
+                <AnchorLink className='btn btn-light' href='#home'>{homeIcon}</AnchorLink>
+                <AnchorLink className='btn btn-light' href='#projects'>{upArrow}</AnchorLink>
+            </div>
             <div className='container-md appSection mx-3-auto my-5'>
                 CONTACT
                 <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
