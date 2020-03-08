128c8cd6e41d837b40e56ff1e26346c74c9dc061
AminesCodes <aminebensalem@pursuit.org>
2020-03-08 12:53:53 -0400

    Card container with vertical margins for images

--- src/assets/projects/projects.js
+++ src/assets/projects/projects.js
@@ -2,32 +2,44 @@ const projects = [
     {
       title: 'Fashion Footprint',
       description: 'A Full-Stack app designed to reduce the error of trends prediction into the Fashion Industry which is the second-most polluter and water consumer worldwide. Fashion Footprint was founded to reduce the overproduction in the Fashion industry is estimate to 30% by allowing customers to choose the trends and styles they want to see in stores for future seasons.',
-      techs: 'Postgres and Express for the backend and Node, React and a mix of Bootstrap and CSS for the frontend',
+      techs: 'Backend: Postgres, Express. Frontend Node, React, mix of Bootstrap and CSS',
+      live: '',
+      github: 'https://github.com/AminesCodes/Fashion-Sustainability',
     },
     {
       title: 'SuitApp',
       description: 'A Full-Stack social media app founded on the idea of bringing together the most passionate and creative minds in the world of Cosplay and giving them the tools to reach a wide audience of fans who appreciate and celebrate their work',
-      techs: 'Postgres and Express for the backend and Node, React and a mix of Bootstrap and CSS for the frontend',
+      techs: 'Backend: Postgres, Express. Frontend Node, React, mix of Bootstrap and CSS',
+      live: '',
+      github: 'https://github.com/AminesCodes/React-Group-Project_Social-Media',
     },
     {
       title: 'Escape The Dungeon',
       description: 'A three level word puzzle game designed to improve vocabulary and spelling skills, where the player guesses a word by clicking letters',
       techs: 'Pure Javascript, HTML and CSS - Frontend application',
+      live: 'https://aminescodes.github.io/Pursuit-Core-Web-Hangman-Weekly-Assignment/',
+      github: 'https://github.com/AminesCodes/Pursuit-Core-Web-Hangman-Weekly-Assignment',
     },
     {
       title: 'Pokemon Battle Simulator',
       description: 'A simple battle simulator that loads random Pokemon form the Pokemon API with their specific information where the player can pick their Pokemon moves to attack the opponent Pokemon played by the computer.',
       techs: 'Pure Javascript, HTML and CSS - Front end application',
+      live: 'https://aminescodes.github.io/Pursuit-Core-Web-Loading-Data-from-Online-Assignment/',
+      github: 'https://github.com/AminesCodes/Pursuit-Core-Web-Loading-Data-from-Online-Assignment',
     },
     {
       title: 'Memory Game',
       description: 'A card game designed to improve the short term memory / visual memory. The game has five levels where the player has to match pars of cards.',
       techs: 'Pure Javascript, HTML and CSS - frontend application',
+      live: 'https://aminescodes.github.io/Memory-Card-Game/',
+      github: 'https://github.com/AminesCodes/Memory-Card-Game',
     },
     {
       title: 'Tic Tac Toe',
       description: 'The world famous game where the player has to align 3 of their marks to win. The game can be played by two players or versus the computer (which will pick moves randomly)',
       techs: 'Pure Javascript, HTML and CSS - frontend application',
+      live: 'https://aminescodes.github.io/Tic-Tac-Toe_HTML-version/',
+      github: 'https://github.com/AminesCodes/Tic-Tac-Toe_HTML-version',
     },
 ]
 
--- src/components/ProjectCardLeft.jsx
+++ src/components/ProjectCardLeft.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 export default function ProjectCardLeft(props) {
     return (
-        <>
-            <div className='card mb-3 bg-dark'>
-                <div className='row no-gutters'>
-                    <div className='col-sm-7'>
-                        <div className='card-body'>
-                            <h5 className='card-title text-center'>{props.project.title}</h5>
-                            <p className='card-text'>{props.project.description}</p>
-                        </div>
-                    </div>
-                    <div className='col-sm-5'>
-                        <img src={props.project.image} className='card-img projectImage my-auto' alt={props.project.image} />
+        <div className='card mb-3 bg-dark'>
+            <div className='row no-gutters'>
+                <div className='col-sm-7'>
+                    <div className='card-body'>
+                        <h5 className='card-title text-center'>{props.project.title}</h5>
+                        <p className='card-text'>{props.project.description}</p>
                     </div>
                 </div>
+                <div className='col-sm-5 d-flex align-items-center justify-content-center'>
+                    <img src={props.project.image} className='card-img projectImage my-auto' alt={props.project.image} />
+                </div>
             </div>
-            <hr />
-        </>
+            <div className='text-center'>
+                Techs: {props.project.techs}
+            </div>
+        </div>
     )
 }
