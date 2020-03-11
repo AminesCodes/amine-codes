fbbcf50b2130b8d58870563ff90de28dd5bc069c
AminesCodes <aminebensalem@pursuit.org>
2020-03-11 02:52:40 -0400

    Added valid links to Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 import { ReactComponent as GithubIcon } from '../assets/nav_icons/github-brands.svg';
 import { ReactComponent as LinkedInIcon } from '../assets/nav_icons/linkedin-brands.svg';
@@ -9,9 +10,15 @@ export default function Footer(props) {
     
     return (
         <div className='fixed-bottom bg-dark d-flex justify-content-center' style={{height: '5vh'}}>
-            <GithubIcon className='smallIcon'/>
-            <LinkedInIcon className='smallIcon'/>
-            <ContactIcon className='smallIcon'/>
+            <a href='https://github.com/AminesCodes' target='_blank' rel='noopener noreferrer'>
+                <GithubIcon className='smallIcon'/>
+            </a>
+            <a href='https://www.linkedin.com/in/amine-bensalem-468791144/' target='_blank' rel='noopener noreferrer'>
+                <LinkedInIcon className='smallIcon'/>
+            </a>
+            <AnchorLink href='#contact'>
+                <ContactIcon className='smallIcon'/>
+            </AnchorLink>
         </div>
     )
 }
