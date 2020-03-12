4d85db675d162f7aacc79ae8f28b9237b7f85cdb
AminesCodes <aminebensalem@pursuit.org>
2020-03-12 00:25:00 -0400

    Home page with front card completed

--- frontend/src/components/HomeCardFront.jsx
+++ frontend/src/components/HomeCardFront.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default (props) => {
+    const [ display, setDisplay ] = useState('visible');
+    setInterval(() => {
+        const time = new Date().toLocaleTimeString();
+        if (time[7] % 2) {
+            setDisplay('visible');
+        } else {
+            setDisplay('invisible');
+        }
+    }, 1000);
+
     return (
         <div className='h-100'>
-            Hi, my name is Amine Bensalem. <br/>
-            I am a software engineer living in New York City.<br/>
-            <span className='textLink' onMouseEnter={() => props.handleHover(true)}>
-                Want to know more about me?
-            </span>
+            <p className='font-weight-bold'>Hello world! <span className={display}>_</span></p>
+            <p>My name is Amine Bensalem. <br/>
+                I am a software engineer <br/>
+                living in New York City.<br/>
+                <span className='textLink' onMouseEnter={() => props.handleHover(true)}>
+                    Want to know more about me?
+                </span>
+            </p>
         </div>
     )
 }
